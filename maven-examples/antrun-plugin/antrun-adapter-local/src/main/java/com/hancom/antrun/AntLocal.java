83ff5419590802015c42eb6838c843f5c914d78a
kenshin579 <kenshin579@gmail.com>
2013-03-06 21:08:00 +0900

    antrun-plugin

---
+++ maven-examples/antrun-plugin/antrun-adapter-local/src/main/java/com/hancom/antrun/AntLocal.java
@@ -0,0 +1,12 @@
+package com.hancom.antrun;
+
+public class AntLocal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Local");
+	}
+
+}
---
+++ maven-examples/antrun-plugin/antrun-adapter-storage/src/main/java/com/hancom/antrun/AntStorage.java
@@ -0,0 +1,12 @@
+package com.hancom.antrun;
+
+public class AntStorage {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Storage");
+	}
+
+}
